c7352f1c030288533900c87bbca40928d516af15
voycebutler18 <voycebutler1@gmail.com>
2025-09-09 16:55:55 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import ErrorBoundary from "@/components/system/ErrorBoundary"; // new file below
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
