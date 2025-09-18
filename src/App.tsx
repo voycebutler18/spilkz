a767147dc4c0112e97ce162df70be17531501704
voycebutler18 <voycebutler1@gmail.com>
2025-09-18 16:22:04 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.tsx
 import NotesPage from "@/pages/Notes";
-import NoteBoxLink from "@/components/NoteBoxLink";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
